3edd9b99317021e526552aab9743febe80703bfc
sethishi <shilpa.sethi@sky.uk>
2017-10-15 08:55:09 +0100

    Completed the singleton pattern

--- src/com/example/designpattern/singletonpattern/DbSingleton.java
+++ src/com/example/designpattern/singletonpattern/DbSingleton.java
@@ -2,15 +2,34 @@ package com.example.designpattern.singletonpattern;
 
 public class DbSingleton {
 
-    public static DbSingleton instance = null;
 
+    /**
+     * Singleton pattern is used to load a class once
+     * mainly used with a static instance of the class
+     * Creational design pattern : Eg :- Spring beans , logger, runtime
+     * instance variable is a private
+     */
+    private static DbSingleton instance = null;
+
+
+    /**
+     * Lazy loading be checking for null
+     */
     public static DbSingleton getInstance() {
         if (instance==null){
-            instance= new DbSingleton();
+
+            synchronized (DbSingleton.class){
+                if(instance==null)
+                    instance= new DbSingleton();
+            }
         }
 
         return instance;
     }
+
+    /**
+     * private so that no other class can use the constructor
+     */
     private DbSingleton(){
 
     }
